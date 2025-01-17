c869ec18b4b41ec7b55f89349b91f9205ba2a4f9
Rashi Agrawal <rashi.agrawal_cs21@gla.ac.in>
2025-01-17 21:50:16 +0530

    Smallest of 3

---
+++ src/Smallest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SmallestNumberExample1
+{
+    public static void main(String[] args)
+    {
+        int a, b, c, smallest, temp;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        a = sc.nextInt();
+        System.out.println("Enter the second number:");
+        b = sc.nextInt();
+        System.out.println("Enter the third number:");
+        c = sc.nextInt();
+        temp=a<b?a:b;
+//comparing the temp variable with c and storing the result in the variable names smallest  
+        smallest=c<temp?c:temp;
+        System.out.println("The smallest number is: "+smallest);
+    }
+}  
